2830239825734ed2f8a2a1930babbe009f2be660
Frameck <fra9879@gmail.com>
2021-10-18 14:39:06 +0200

    Completed exercise - palindrome

---
+++ palindromi/js/script.js
@@ -0,0 +1,23 @@
+function isPalindrome(word) {
+    let array = word.split('')
+    let arrayReverse = array.reverse()
+    if (arrayReverse.join('') === word) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+const word = prompt('Inserisci una parola per verificare se è palindroma')
+
+const wordIsPalindrome = isPalindrome(word)
+let message = ''
+
+if (wordIsPalindrome) {
+    message = 'La parola fornita è palindroma'
+} else {
+    message = 'La parola fornita non è palindroma'
+}
+
+alert(message)
